b5444be15a122f74254c1e572ceae08bfe2ab081
Meshack Daniels <mannyzyg@gmail.com>
2025-10-03 15:13:22 +0100

    Update layout.tsx removed the google track

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,16 +10,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>1960</title>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-770360780">
-        </script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'AW-770360780');
-        </script>
       </head>
       <body className="bg-cream">{children}</body>
     </html>
