41a4abad5bc299e8fe7364db4519dcdb53048d2f
Meshack Daniels <mannyzyg@gmail.com>
2026-01-10 21:40:13 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import Script from 'next/script';
+
 import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({
@@ -10,15 +10,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-     
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17622013112"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-17622013112');
-</script>
         
         <title>1960</title>
       </head>
