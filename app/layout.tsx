b6604fb6547002b97b2287f82d866e36c7e0eeb0
Meshack Daniels <mannyzyg@gmail.com>
2026-01-10 21:25:22 +0000

    Update layout.tsx
    removed the whole gtag thing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Script from 'next/script'; // Add this import
+
 
 export default function RootLayout({
   children,
@@ -10,22 +10,7 @@ export default function RootLayout({
       <head /> {/* Optional: For other head elements */}
       <body>
         {children}
-        {/* Google Ads gtag.js script */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17622013112"
-          strategy="afterInteractive" // Loads after page hydration
-        />
-        <Script
-          id="google-ads" // Unique ID for the script
-          strategy="afterInteractive"
-        >
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17622013112');
-          `}
-        </Script>
+        
       </body>
     </html>
   );
