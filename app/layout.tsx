4b0feeac5083e99364e4d025dec094a82f1e4c53
Meshack Daniels <mannyzyg@gmail.com>
2026-01-10 21:35:14 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head /> {/* Optional: For other head elements */}
+      
       <body>
         {children}
         
